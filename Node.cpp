0d38c6634ef835008cad518b7b046434a1a7ff2b
Wojciech Domagalski <domagalskiwojtek@gmail.com>
2025-12-11 16:11:26 +0100

    Added latency to edges

--- Node.cpp
+++ Node.cpp
@@ -5,6 +5,7 @@ Node::Node(int id) : id(id) {}
 
 void Node::addNeighbor(int n) {
     neighbors.push_back(n);
+    linkDelay.push_back(delay);
 }
 
 void Node::getMessage(int from, std::queue<int>& q) {
